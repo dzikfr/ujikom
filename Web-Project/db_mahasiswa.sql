8bd5d815798c5fb8377f28753dec1b9b7b9851e0
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2025-03-16 12:06:22 +0700

    adding db dump

---
+++ Web-Project/db_mahasiswa.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 16, 2025 at 06:02 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_mahasiswa`
+--
+
+CREATE TABLE `table_mahasiswa` (
+  `nim` bigint(20) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `jurusan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `table_mahasiswa`
+--
+
+INSERT INTO `table_mahasiswa` (`nim`, `nama`, `gender`, `jurusan`) VALUES
+(211011450092, 'dzikri fauzi ramdhani', 'Laki-Laki', 'Teknik Informatika'),
+(120, 'orang1', 'Laki-Laki', 'Sistem Informasi');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
